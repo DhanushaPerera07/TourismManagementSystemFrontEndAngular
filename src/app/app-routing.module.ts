067ddc24a0ebdbb92f73a6bf3cb5947cba5a173c
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-02-20 22:30:12 +0530

    Design the main header component (main-nav).
    Create sign-in component.
    Add logo and icons needed for the header.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {MainNavComponent} from './main-nav/main-nav.component';
+import {SignInComponent} from './signin/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: MainNavComponent},
+  {path: 'signin', component: SignInComponent},
+  {path: '', redirectTo: '/signin', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { SignInComponent } from './signin/sign-in.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

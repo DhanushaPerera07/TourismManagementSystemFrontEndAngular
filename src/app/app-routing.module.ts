c601f8a9ba92c0d86aef58d311992b763aa4c30b
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-02-21 09:37:06 +0530

    Create not-found component. Config basic routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,31 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {MainNavComponent} from './main-nav/main-nav.component';
 import {SignInComponent} from './signin/sign-in.component';
+import {CustomerComponent} from './customer/customer.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 
 const routes: Routes = [
-  {path: 'home', component: MainNavComponent},
-  {path: 'signin', component: SignInComponent},
-  {path: '', redirectTo: '/signin', pathMatch: 'full'}
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: MainNavComponent
+  },
+  {
+    path: 'customer',
+    component: CustomerComponent
+  },
+  {
+    path: 'signin',
+    component: SignInComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainNavComponent } from './main-nav/main-nav.component';
-import { SignInComponent } from './signin/sign-in.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CustomerComponent } from './customer/customer.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MainNavComponent} from './main-nav/main-nav.component';
+import {SignInComponent} from './signin/sign-in.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CustomerComponent} from './customer/customer.component';
+import {MatDividerModule} from '@angular/material/divider';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainNavComponent,
     SignInComponent,
-    CustomerComponent
+    CustomerComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
